8615e649c2349fd73d6a9c986f13e3030f796a74
Bhavanish <99014663+Bhavanish-git@users.noreply.github.com>
2022-02-23 11:52:09 +0530

    generics assignment complete

---
+++ UML & Java/GenericAssignment/Generic_3.java
@@ -0,0 +1,24 @@
+package GenericAssignment;
+
+class Generic<T>{
+	public T[] swap(T[] a, int i, int j) {
+        T temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        return a;
+	}
+	
+}
+public class Generic_3 {
+
+	public static void main(String[] args) {
+		Integer arr[] = {1,2,3,4,5};
+		Generic<Integer> objGeneric = new Generic<>();
+		Integer[] arr1=objGeneric.swap(arr, 0, 3);
+		for(int i : arr1) {
+			System.out.println(i);
+		}
+
+	}
+
+}
