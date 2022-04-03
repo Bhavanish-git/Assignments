27d28cea31ca19f76a13bc0465bb9b5e04085a05
Bhavanish <99014663+Bhavanish-git@users.noreply.github.com>
2022-04-03 22:48:42 +0530

    Javascript 15 qns complete

---
+++ Javascript/Page4Q2.js
@@ -0,0 +1,3 @@
+var username = prompt ("whats ur name");
+
+document.write ("hey " + username + " Have a nice day buddy !");
---
+++ Javascript/Page4Q3.js
@@ -0,0 +1,11 @@
+var username = prompt ("whats ur name");
+
+if(username == "Alice" || username == "Bob"){
+
+document.write ("Hey " + username + " Have a nice day buddy !");
+
+}
+
+else {
+    document.write("Sorry "+ username + " I dont even know who you are");
+}
